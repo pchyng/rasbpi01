677f0bcdae91d903dad34523f124f80efd29b8a0
pchyng <pedro258@gmail.com>
2023-05-04 15:56:29 +0200

    base menu update

--- base.py
+++ base.py
@@ -1,5 +1,17 @@
 import time
 
-while True:
-	print("test")
-	time.sleep(2)
+bContinue = True
+
+while bContinue:
+	choice = input("""
+1.Time Lapse
+2.Stop process
+3.Exit/Quit
+Please enter choice: """)
+	if choice == '1':
+		print("choice 1")
+	elif choice == '2':
+		print("choice 2")
+	elif choice == '3':
+		print("choice 3")
+		bContinue = False
